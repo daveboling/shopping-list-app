5319fe4eac42c7513bbd724a07456a1043be1226
Dave Boling <daviddboling@gmail.com>
2014-02-11 01:16:19 -0500

    Improved aesthetics, added backdrop, JS done!
    JavaScript & jQuery are done. Form validation is complete, now it's time
    to make this thing sexy!

--- js/shop.js
+++ js/shop.js
@@ -11,39 +11,40 @@ var editable = $(".editable");
 
 //List Area
 var list = $(".list");
-var testDiv = $(".title"); //For debugging
+//var testDiv = $(".title"); //For debugging
 
-//Making Quantity, Price, and all inputs that aren't defiend with classes
-input.numeric(); //Not functional
+//Making Quantity, Price, Editable take only numbers
+//Uses jquery.numeric plugin
+editable.numeric();
 quantity.numeric();
 price.numeric();
 
 
 
-//Add an item to the list and it's respective price, quantity, total, and item type
-
+	//Add Button
 	$(add).on('click', function (){ 
+		//Get a current total for the append
 		var totalPandQ = price.val() * quantity.val();
+
+
 		//First, you need to check if the forms are blank!
-		if (item.val() === "" || quantity.val() === "" || price.val() === ""){
+		if (item.val() === "" || quantity.val() === ""){
 		}
+
 		//Else, add the stuff yo.
 		else {
 			//Add it to HTML
-			list.append('<div class="listItem"> ' + item.val() + '<div class="itemPrice">' + price.val() + '</div><div class="itemQuantity">' + quantity.val() + '</div><div class="itemTotal">' + totalPandQ + '</div><div class="delete">Delete</div><div class="edit">Edit</div><div class="save">Save</div></div>');
-			
-			//Add up current total
-			calcTotal();
+			list.append('<div class="listItem"> ' + item.val() + '<div class="itemPrice"><span class="num">3</span><input class="editable" input="text"></div><div class="itemQuantity"><span class="quant">3</span><input class="editable" input="text"></div><div class="itemTotal">' + totalPandQ + '</div><div class="delete">Delete</div><div class="edit">Edit</div><div class="save">Save</div></div>');
 		}
 
-
+		//Add up current total
+		calcTotal();
 	});
 
 
 	/* Note: on(event, selector, object handler) */
 	//Delete Button
 	$(document).on('click', '.delete', function () {
-		testDiv.text($('.itemType') + 'was deleted.');
 		$(this).closest('.listItem').remove();
 		calcTotal();
 	});
@@ -51,38 +52,37 @@ price.numeric();
 
 	//Edit Button
 	$(document).on('click', '.edit', function () {
-		testDiv.text('Edit was clicked');
 		$(this).closest('.listItem').find('.edit').hide();
 		$(this).closest('.listItem').find('.delete').hide();
 		$(this).closest('.listItem').find('.save').show();
 
 		//Using text() here since we are dealing with divs, probably something better to be used
 		//here, but it works like I want it to.
-		var currentPrice = $(this).closest('.listItem').find('.itemPrice').text();
-		var currentQuantity = $(this).closest('.listItem').find('.itemQuantity').text();
+		var currentPrice = +($(this).closest('.listItem').find('.num').text());
+		var currentQuantity = +($(this).closest('.listItem').find('.quant').text());
 
-		//Price
+		//Hide Price
 		$(this).closest('.listItem')
-		.find('.itemPrice')
-		.html('<input class="editable" type="text" min="0" value="' + currentPrice + '">');
-
-		//Quantity
+		.find('.num').text('');
 		$(this).closest('.listItem')
-		.find('.itemQuantity')
-		.html('<input class="editable" type="text" min="0" max="99" value="' + currentQuantity + '">');
+		.find('.quant').text('');
 
-
-
-
+		//Show Price Edit Box
+		$(this).closest('.listItem')
+		.find('.editable').val(currentPrice).show();
 	});
 
 	//Save Button
 	$(document).on('click', '.save', function () {
+
 		//Hide and show selected buttons
 		$(this).closest('.listItem').find('.edit').show();
 		$(this).closest('.listItem').find('.delete').show();
 		$(this).closest('.listItem').find('.save').hide();
 
+		//Hide the forms
+		$(this).closest('.listItem')
+		.find('.editable').hide();
 
 		//Here, change() needs to be used to detect what has changed about the current form fields
 		//Since we are working with forms, we can use val() to get the values
@@ -92,15 +92,21 @@ price.numeric();
 		var newItemTotal = newPrice.val() * newQuantity.val();
 	
 
+		/* Note, I made the text() blank in these spans
+		   so that hide() wouldn't save the Save button
+		   to show blank text as if the span had been deleted.
+		*/
+
+
 		//New Price
 		$(this).closest('.listItem')
-		.find('.itemPrice')
-		.html(newPrice.val());
+		.find('.num')
+		.text(newPrice.val());
 
 		//New Quantity
 		$(this).closest('.listItem')
-		.find('.itemQuantity')
-		.html(newQuantity.val());
+		.find('.quant')
+		.text(newQuantity.val());
 
 		//New Total
 		$(this).closest('.listItem')
